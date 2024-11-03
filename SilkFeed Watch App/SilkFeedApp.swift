194f5390d50d6cd0e979c28824c6556f7027a6ba
YuKun-Li-Swift <wzbnzlwv@outlook.com>
2024-11-03 16:12:43 +0800

    Initial Commit

---
+++ SilkFeed Watch App/SilkFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  SilkFeedApp.swift
+//  SilkFeed Watch App
+//
+//  Created by Yukun Li on 2024/11/3.
+//
+
+import SwiftUI
+
+@main
+struct SilkFeed_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
